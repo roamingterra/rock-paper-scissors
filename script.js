6304ad2421a8f529d493690c0e8afd56fc4fdd7b
roamingterra <danielballerini1@gmail.com>
2022-10-31 11:50:41 -0400

    Move helper functions to global scope
    Made the helper functions go in the global scope and at the bottom in
    order to follow a flow of most important to least important. I also
    moved the initializations of the player and computer scores to the
    global scope do that these functions could access them.

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function getComputerChoice(){
 // Randomly generate a number between 1 and 3, and store number in a variable
     let compAction = Math.floor(Math.random()*3)+1;
 // Assign 1, 2, and 3 to an action ('rock', 'paper', or 'scissors') (conditional statement)
-    if(compAction===1){
-        compAction = "rock";
-    } else if(compAction===2){
-        compAction = "paper";
-    } else if(compAction===3){
-        compAction = "scissors";
-    }
+        if(compAction===1){
+            compAction = "rock";
+        } else if(compAction===2){
+            compAction = "paper";
+        } else if(compAction===3){
+            compAction = "scissors";
+        }
 // Return and print to console the computer's action
     console.log(`The computer chose ${compAction}`);
     return compAction;
     }
 
-// 4. WRITE A FUNCTION THAT PLAYS A SINGLE ROUND OF ROCK, PAPER, SCISSORS, THAT TAKES IN AS PARAMETERS THE PLAYER ACTION AND THE COMPUTER ACTION
+// 4. PLAY A SINGLE ROUND OF ROCK, PAPER, SCISSORS, THAT TAKES IN AS PARAMETERS THE PLAYER ACTION AND THE COMPUTER ACTION
 function playGame(playerAction, compAction){
 // Make the user's choice case insensitive
     playerAction = playerAction.toLowerCase();
@@ -41,36 +41,53 @@ function playGame(playerAction, compAction){
 }
 
 // 6. WRITE A NEW FUNCTION WHICH WILL CALL THE PLAY GAME FUNCTION TO PLAY A FIVE ROUND GAME WHICH KEEPS SCORE AND ANNOUNCES A WINNER
-function game(){
-
-    //HELPER FUNCTION: PRINT THE SCORES OF THE GAME
-    let printScores = () => console.log(`Player Score: ${playerScore} Computer Score: ${compScore}`);
-
-    //HELPER FUNCTION: FINAL RESULT OF ALL FIVE ROUNDS
-    function printFinalResult () {
-        if(playerScore>compScore) console.log("Congratulations, you win!");
-        else if (playerScore<compScore) console.log("You lose! Try again next time!");
-        else console.log("The game ended in a tie!");
-    }
-
-    // Declare variable to record player and computer score
-    let playerScore = 0; 
-    let compScore = 0;
+function game(){   
     // Loop through the game 5 times
     for(let i = 0; i < 5; i++){
         // Ask for the user's move and store it in a variable.
-        const playerAction = prompt("Make a move: ");
+        let playerAction = prompt("Make a move: ");
         // Call the computerChoice function and store the resulting move in a variable
-        const compAction = getComputerChoice();
-        // Call the playGame function and record the score
-        let round = playGame(playerAction, compAction);
-        if(round==1) playerScore++;
-        else if (round==-1) compScore++;
+        compAction = getComputerChoice();
+        // Record the score
+        let roundResult = playGame(playerAction, compAction);
+            if(roundResult==1) {
+                playerScore++;
+            }
+            else if (roundResult==-1) {
+                compScore++;
+            }
+        // Print the score
         printScores();
     }
     // Return the result of the five rounds 
-    printFinalResult()
+    printFinalResult();
 }
 
+//HELPER FUNCTION 1: PRINT THE SCORES OF THE GAME
+ function printScores(){
+    console.log(`Player Score: ${playerScore} Computer Score: ${compScore}`);
+}
+
+ //HELPER FUNCTION 2: PRINT FINAL RESULT OF ALL FIVE ROUNDS
+ function printFinalResult () {
+     if(playerScore>compScore){
+         console.log("Congratulations, you win!");
+     }
+     else if(playerScore<compScore) {
+     console.log("You lose! Try again next time!");
+     }
+     else {
+         console.log("The game ended in a tie!");
+     }
+ }
+ 
 // 7. TEST THE SCRIPT
+
+// Declare variables to record player and computer actions and scores
+let playerScore = 0; 
+let compScore = 0;
+
+// Let's play 5 rounds
 game();
+
+   
